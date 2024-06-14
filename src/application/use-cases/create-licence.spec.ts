d303c33e196305a3798e9895fc07d9a175b74206
devErickBR <erickyanSP@outlook.com>
2024-06-14 06:48:46 -0300

    adicionar a arquitetura either

--- src/application/use-cases/create-licence.spec.ts
+++ src/application/use-cases/create-licence.spec.ts
@@ -14,13 +14,16 @@ describe('Create Licence with use case', () => {
         const licence = new CreateLicence(licenceRepo);
         const result = await licence.execute(licenceProps);
 
-        expect(result).instanceOf(Licence);
-        expect(result.name).toBe(licenceProps.NAME_LICENCE);
-        expect(result.id).toBe(licenceProps.ID_LICENCE);
-        expect(result.duration).toBe(licenceProps.DURATION_DAYS);
+        expect(result.isRight()).toBe(true);
+        if (result.isRight()) {
+            expect(result.value).instanceOf(Licence);
+            expect(result.value.name).toBe(licenceProps.NAME_LICENCE);
+            expect(result.value.id).toBe(licenceProps.ID_LICENCE);
+            expect(result.value.duration).toBe(licenceProps.DURATION_DAYS);
+        }
     });
 
-    it('should be able deny creation an licence, case duration smaller 0', async () => {
+    it('should be able deny creation an licence, case duration smaller than 0', async () => {
         const licenceProps = {
             ID_LICENCE: 1,
             NAME_LICENCE: 'teste',
@@ -28,11 +31,15 @@ describe('Create Licence with use case', () => {
         };
         const licenceRepo = new InMemoryLicencesRepository();
         const licence = new CreateLicence(licenceRepo);
-
-        expect(async () => await licence.execute(licenceProps)).rejects.toThrow(
-            'not possible create Licence with duration smaller 0',
-        );
+        const result = await licence.execute(licenceProps);
+        expect(result.isLeft()).toBe(true);
+        if (result.isLeft()) {
+            expect(result.value.message).toBe(
+                'not possible create Licence with duration smaller 0',
+            );
+        }
     });
+
     it('should be able deny creation an licence,if the id already exist', async () => {
         const licenceProps = {
             ID_LICENCE: 1,
@@ -41,13 +48,17 @@ describe('Create Licence with use case', () => {
         };
         const licenceRepo = new InMemoryLicencesRepository();
         const licence = new CreateLicence(licenceRepo);
-        const result = await licence.execute(licenceProps);
-        licenceRepo.licences.push(result);
-
-        expect(async () => await licence.execute(licenceProps)).rejects.toThrow(
-            'ID already exist',
-        );
+        let result = await licence.execute(licenceProps);
+        if (result.isRight()) {
+            licenceRepo.licences.push(result.value);
+        }
+        result = await licence.execute(licenceProps);
+        expect(result.isLeft()).toBe(true);
+        if (result.isLeft()) {
+            expect(result.value.message).toBe('ID already exist');
+        }
     });
+
     it('should be able deny creation an licence,if the licence name already exist', async () => {
         const licenceProps = {
             ID_LICENCE: 3,
@@ -56,16 +67,22 @@ describe('Create Licence with use case', () => {
         };
         const licenceRepo = new InMemoryLicencesRepository();
         const licence = new CreateLicence(licenceRepo);
-        const result = await licence.execute(licenceProps);
-        licenceRepo.licences.push(result);
+        let result = await licence.execute(licenceProps);
+        if (result.isRight()) {
+            licenceRepo.licences.push(result.value);
+        }
 
-        expect(
-            async () =>
-                await licence.execute({
-                    DURATION_DAYS: 30,
-                    NAME_LICENCE: 'test',
-                    ID_LICENCE: 1,
-                }),
-        ).rejects.toThrow('the license name already exists');
+        result = await licence.execute({
+            ID_LICENCE: 5,
+            NAME_LICENCE: 'teste',
+            DURATION_DAYS: 30,
+        });
+
+        expect(result.isLeft()).toBe(true);
+        if (result.isLeft()) {
+            expect(result.value.message).toBe(
+                'the license name already exists',
+            );
+        }
     });
 });
--- src/application/use-cases/create-licence.ts
+++ src/application/use-cases/create-licence.ts
@@ -1,5 +1,6 @@
 import { LicenceRepository } from './../repositories/licence-repository';
 import { Licence } from '../../domain/entities/licence';
+import { Either, left, right } from '../../domain/errors/either';
 
 interface CreateLicenceRequest {
     ID_LICENCE?: number;
@@ -7,33 +8,37 @@ interface CreateLicenceRequest {
     DURATION_DAYS: number;
 }
 
-type CreateLicenceResponse = Promise<Licence>;
+type CreateLicenceResponse = Licence;
+
+type Response = Either<Error, CreateLicenceResponse>;
 
 export class CreateLicence {
     constructor(private licenceRepository: LicenceRepository) {}
 
-    async execute(props: CreateLicenceRequest): CreateLicenceResponse {
+    async execute(props: CreateLicenceRequest): Promise<Response> {
         if (props.ID_LICENCE) {
             if (await this.licenceRepository.findById(props.ID_LICENCE)) {
-                throw new Error('ID already exist');
+                return left(new Error('ID already exist'));
             }
         }
 
         if (props.NAME_LICENCE) {
             if (await this.licenceRepository.findByName(props.NAME_LICENCE)) {
-                throw new Error('the license name already exists');
+                return left(new Error('the license name already exists'));
             }
         }
 
         if (props.DURATION_DAYS < 0) {
-            throw new Error(
-                'not possible create Licence with duration smaller 0',
+            return left(
+                new Error(
+                    'not possible create Licence with duration smaller 0',
+                ),
             );
         }
 
         const id = props.ID_LICENCE || undefined;
         const licence = new Licence({ ID_LICENCE: id, ...props });
 
-        return licence;
+        return right(licence);
     }
 }
