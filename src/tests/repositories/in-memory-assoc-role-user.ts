22556e15781f90f9e5676e724a77cac73a8ef235
DevErickBR <erickyanSP@Outlook.com>
2024-06-17 22:02:35 -0300

    adicionar contrato de reposito de assoc-role-group

---
+++ src/tests/repositories/in-memory-assoc-role-user.ts
@@ -0,0 +1,29 @@
+import { AssocRoleUserRepository } from "../../application/repositories/assoc-role-user";
+import { AssocRoleUser } from "../../domain/entities/assoc-role-user";
+
+export class InMemoryAssocRoleUser implements AssocRoleUserRepository {
+    public assocs: AssocRoleUser[] = []
+
+    async findByAssoc(assoc: AssocRoleUser): Promise<AssocRoleUser | null> {
+        const result = this.assocs.find((props) => props.getIdRole === assoc.getIdRole && props.getIdUser === assoc.getIdUser)
+
+        return result ?? null
+    }
+
+    async findByRole(id: number): Promise<AssocRoleUser | null> {
+        const result = this.assocs.find((props) => props.getIdRole === id)
+
+        return result ?? null
+    }
+
+    async findByUser(id: string): Promise<AssocRoleUser | null> {
+        const result = this.assocs.find((props) => props.getIdUser === id)
+
+        return result ?? null
+    }
+
+    async save(assoc: AssocRoleUser): Promise<void> {
+        this.assocs.push(assoc);
+
+    }
+}
